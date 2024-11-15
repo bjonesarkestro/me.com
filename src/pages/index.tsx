a3331f16bd8b707e0e57f8e26fc914a9a9e71c7e
Brandon Jones <brandon.jones@arkestro.com>
2024-11-15 16:16:14 -0500

    Add files

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'; 
+import Head from 'next/head'
+
+const Home: FC = () => { 
+  return (
+    <>
+    <Head>
+      <title>Your Name</title>
+    </Head>
+    <main className="flex flex-col items-center justify-between min-h-0 p-2">
+    </main>
+    </>
+  )
+}
